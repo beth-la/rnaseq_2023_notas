02c7faec67bd2568c070fb8dcbc13c6d620e72a7
Brenda <brenda.titanium@hotmail.com>
2023-01-31 13:26:12 -0600

    Initial commit

---
+++ R/Bioconductor_new_packages.R
@@ -0,0 +1,14 @@
+## Bioconductor newest software packages:
+# Biological Network Analysis in R
+# Autores: Colin Mclean, Anatoly Sorokin, T. I. Simpson, J. Douglas Armstrong, Oksana Sorokina
+# Llamado también BioNAR es un paquete cuyo objetivo principal es cuantificar y clasificar cada
+# impacto simultaneo de proteínas en redes complejas, basado en su topologia y clustering.
+# Una caracteristica bastante interesante es que este paquete permite estimar la co-ocurrencia de
+# enfermedades a traves de la red y clusters especificos, apuntando a mecanismos comunes o compartidos.
+# ¿Se han respondido las preguntas hechas a los autores?
+# Este paquete no muestra tantas preguntas hechas por los usuarios, para el día en el que fue consultado este
+# sitio, había poco mas de 50 preguntas, de las cuales, casi la mitad se encontraban sin respuesta, por otro lado
+# algunas de ellas habian sido publicadas pocas horas antes de esta consulta, por lo que sería errado concluir que
+# si existe o no respuesta por los autores.
+
+# Consensus Independent Component Analysis
