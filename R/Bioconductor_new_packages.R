bb5fe72990452968d9777df33414d5583c018ca3
Brenda Elizabeth Lopez Angeles <blopez@lcg.unam.mx>
2023-01-31 15:24:26 -0600

    Version final

--- R/Bioconductor_new_packages.R
+++ R/Bioconductor_new_packages.R
@@ -1,4 +1,7 @@
-## Bioconductor newest software packages:
+## Title: Bioconductor newest software packages:
+## Author: Lopez Angeles B. Elizabeth
+## Date:
+
 # Biological Network Analysis in R
 # Autores: Colin Mclean, Anatoly Sorokin, T. I. Simpson, J. Douglas Armstrong, Oksana Sorokina
 # Llamado también BioNAR es un paquete cuyo objetivo principal es cuantificar y clasificar cada
@@ -6,9 +9,28 @@
 # Una caracteristica bastante interesante es que este paquete permite estimar la co-ocurrencia de
 # enfermedades a traves de la red y clusters especificos, apuntando a mecanismos comunes o compartidos.
 # ¿Se han respondido las preguntas hechas a los autores?
-# Este paquete no muestra tantas preguntas hechas por los usuarios, para el día en el que fue consultado este
-# sitio, había poco mas de 50 preguntas, de las cuales, casi la mitad se encontraban sin respuesta, por otro lado
-# algunas de ellas habian sido publicadas pocas horas antes de esta consulta, por lo que sería errado concluir que
-# si existe o no respuesta por los autores.
+#   Este paquete no muestra tantas preguntas hechas por los usuarios, para el día en el que fue consultado este
+#   sitio, había poco mas de 50 preguntas, de las cuales, casi la mitad se encontraban sin respuesta, por otro lado
+#   algunas de ellas habian sido publicadas pocas horas antes de esta consulta, por lo que sería errado concluir que
+#   si existe o no respuesta por los autores. Es bastante importante destacar que ninguna pregunta se asocia a problemas
+#   con la instalación y/o sistema operativo.
 
-# Consensus Independent Component Analysis
+# DOCUMENTACION
+#   A simple vista podemos decir sin duda alguna que se trata de un
+#   manual bastante util y amigable con el usuario, en el se incluyen algunas imagenes y se encuentra gerarquisado y acomodado
+#   de una manera que lo hace visualmente agradable de leer.
+
+# DATOS
+#   Rank: 2151/2183
+#   in Bioc: <6 months
+#   Dependencias: 169
+
+
+
+
+
+
+
+
+
+
