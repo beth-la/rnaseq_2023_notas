cf104f5f200cd504d9381ce3e027b7285bf2f98f
Brenda Elizabeth Lopez Angeles <blopez@lcg.unam.mx>
2023-02-01 15:19:30 -0600

    Agregando comentarios al codigo y corrigiendo algunos errores de redaccion.

--- R/Feb01_NotesRecount3.R
+++ R/Feb01_NotesRecount3.R
@@ -1,20 +1,17 @@
-## ----'start', message=FALSE--------
-## Load recount3 R package
+# EJERCICIO CON RECOUNT Y iSEE
+## Ejercicio realizado en clase.
+
+# Cargamos la libreria que se utilizara:
 library("recount3")
 
-
-## ----'quick_example'---------------
 ## Cambiaremos el URL de recount3 a Amazon (AWS)
-## para evitar problems de TLS versión 1.2 en
-## Windows (luego tiene una versión más vieja).
-
 ## Primero vemos el URL actual
 getOption(
   "recount3_url",
   "http://duffel.rail.bio/recount3"
 )
 
-## Ahora si lo cambiamos
+## Ahora si lo cambiamos, esto se hizo paraa evitar problemas con algunas versiones de windows
 options(recount3_url = "https://recount-opendata.s3.amazonaws.com/recount3/release")
 
 ## Y vemos que ya funcionó el cambio.
@@ -23,8 +20,12 @@ getOption(
   "http://duffel.rail.bio/recount3"
 )
 
-## Revisemos todos los proyectos con datos de humano en recount3
-human_projects <- available_projects()
+# Utilizamos recount para ver los proyectos: Agregando la URL
+human_projects <- available_projects(recount3_url = "https://recount-opendata.s3.amazonaws.com/recount3/release")
+
+# Descargando archivos:
+# adding rname 'https://recount-opendata.s3.amazonaws.com/recount3/release/human/data_sources/sra/metadata/sra.recount_project.MD.gz'
+# adding rname 'https://recount-opendata.s3.amazonaws.com/recount3/release/human/data_sources/tcga/metadata/tcga.recount_project.MD.gz'
 
 ## Encuentra tu proyecto de interés. Aquí usaremos
 ## SRP009615 de ejemplo
@@ -32,32 +33,26 @@ proj_info <- subset(
   human_projects,
   project == "SRP009615" & project_type == "data_sources"
 )
+
 ## Crea un objetio de tipo RangedSummarizedExperiment (RSE)
 ## con la información a nivel de genes
 rse_gene_SRP009615 <- create_rse(proj_info)
-## Explora el objeto RSE
+
+# Explorar objeto
 rse_gene_SRP009615
 
+## Explora los proyectos disponibles de forma interactiva
+proj_info_interactive <- interactiveDisplayBase::display(human_projects)
+## Selecciona un solo renglón en la tabla y da click en "send".
 
-## ----"interactive_display", eval = FALSE----
-## ## Explora los proyectos disponibles de forma interactiva
-## proj_info_interactive <- interactiveDisplayBase::display(human_projects)
-## ## Selecciona un solo renglón en la tabla y da click en "send".
-##
-## ## Aquí verificamos que solo seleccionaste un solo renglón.
-## stopifnot(nrow(proj_info_interactive) == 1)
-## ## Crea el objeto RSE
-## rse_gene_interactive <- create_rse(proj_info_interactive)
+## Crea el objeto RSE
+rse_gene_interactive <- create_rse(proj_info_interactive)
 
-
-## ----"tranform_counts"-------------
 ## Convirtamos las cuentas por nucleotido a cuentas por lectura
 ## usando compute_read_counts().
 ## Para otras transformaciones como RPKM y TPM, revisa transform_counts().
 assay(rse_gene_SRP009615, "counts") <- compute_read_counts(rse_gene_SRP009615)
 
-
-## ----"expand_attributes"-----------
 ## Para este estudio en específico, hagamos más fácil de usar la
 ## información del experimento
 rse_gene_SRP009615 <- expand_sra_attributes(rse_gene_SRP009615)
@@ -66,3 +61,6 @@ colData(rse_gene_SRP009615)[
   grepl("^sra_attribute", colnames(colData(rse_gene_SRP009615)))
 ]
 
+# Observamos lo obtenido con iSEE y generamos una grafica.
+library("iSEE")
+iSEE::iSEE(rse_gene_SRP009615)
